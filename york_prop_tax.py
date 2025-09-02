9a93d2e5f9fdd25525004adf6cac8a36875279c4
Keith Kyzivat <kamaji@gmail.com>
2025-09-01 22:19:14 -0400

    Add option for searching pre-downloaded pdf
    This adds option --search-pdf to specify the york property tax pdf that
    contains all tax builds. This skips the selenium web search and file
    download.

--- york_prop_tax.py
+++ york_prop_tax.py
@@ -6,6 +6,23 @@ from pypdf import PdfWriter
 import urllib
 import sys
 
+def process_pdf(pdf_io, search_query, output):
+    if search_query:
+        found_page = find_page(pdf_io, search_query)
+        if found_page:
+            writer = PdfWriter()
+            writer.add_page(found_page)
+            if output:
+                with open(output, "wb") as outfile:
+                    writer.write(outfile)
+        else:
+            print(f"No page found for query '{search_query}'", file=sys.stderr)
+    else:
+        if output:
+            with open(output, "wb") as outfile:
+                outfile.write(pdf_io.read())
+        else:
+            sys.stdout.buffer.write(pdf_io.read())
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -22,29 +39,26 @@ if __name__ == "__main__":
         help="A search query used to find the desired property tax bill page.")
     parser.add_argument("-o", "--output", metavar="output-path", type=Path,
         default=None, help="Output PDF file.")
+    parser.add_argument("--search-pdf", metavar="PDF_FILE", type=str,
+        default=None,
+        help="Provide an already downloaded PDF of all tax bills")
     args = parser.parse_args()
     cmd = args.command
     search_query = args.search.lower() if args.search else None
 
     try:
-        tb_url = get_tax_bill_pdf()
+        if args.search_pdf:
+            if not search_query:
+                print("Existing PDF, no search query, doing nothing.")
+                sys.exit(0)
 
-        pages = None
-        with urllib.request.urlopen(tb_url) as pdf_file:
-            writer = PdfWriter()
-            if search_query:
-                writer.add_page(find_page(pdf_file, search_query))
-                if args.output:
-                    with open(args.output, "wb") as outfile:
-                        writer.write(outfile)
-                        writer.close()
-                else:
-                    writer.write(sys.stdout)
-            elif args.output:
-                with open(args.output, "wb") as outfile:
-                    outfile.write(pdf_file.read())
-            else:
-                sys.stdout.buffer.write(pdf_file.read())
+            with open(args.search_pdf, "rb") as pdf_io:
+                process_pdf(pdf_io, search_query, args.output)
+        else:
+            tb_url = get_tax_bill_pdf()
+            pages = None
+            with urllib.request.urlopen(tb_url) as pdf_response:
+                process_pdf(pdf_response, search_query, args.output)
 
     except BillNotFoundException as e:
         print(f"{str(e)}... aborting", file=sys.stderr)
